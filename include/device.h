1405a4c2d91418fa9365132c1fc26fec9bc71307
Erik Price <erik.price16@gmail.com>
2013-02-10 22:10:50 -0600

    Ignore noisy TIMEOUT error

--- include/device.h
+++ include/device.h
@@ -15,7 +15,9 @@ const char* nvml_error_code_string(nvmlReturn_t ret);
 // Returns non-zero on error, 0 otherwise
 static inline int nvml_try(nvmlReturn_t ret, const char* fn)
 {
-  if(ret != NVML_SUCCESS) {
+  // We ignore the TIMEOUT error, as it simply indicates that
+  // no events (errors) were triggered in the given interval.
+  if(ret != NVML_SUCCESS && ret != NVML_ERROR_TIMEOUT) {
     fprintf(stderr, "%s: %s: %s\n", fn, nvml_error_code_string(ret),
             nvmlErrorString(ret));
     return 1;
