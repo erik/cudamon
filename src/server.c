8a00e7a898c082e751881fae54c985feb999d998
Erik Price <erik.price16@gmail.com>
2013-02-12 12:55:34 -0600

    Begin sending AJAX replies

--- src/server.c
+++ src/server.c
@@ -13,12 +13,40 @@ static const char* MG_OPTIONS[] = {
   NULL
 };
 
+static const char* ajax_reply_start =
+  "HTTP/1.1 200 OK\r\n"
+  "Cache: no-cache\r\n"
+  "Content-Type: application/x-javascript\r\n"
+  "\r\n";
+
+static struct monitor* monitor = NULL;
+
+static void ajax_send_update(struct mg_connection *conn)
+{
+  mg_printf(conn, "%s", ajax_reply_start);
+
+  mg_printf(conn, "{");
+  mg_printf(conn, "'devices' : [");
+
+  for(unsigned i = 0; i < monitor->dev_count; ++i) {
+    struct device dev = monitor->devices[i];
+
+    mg_printf(conn, "'temperature':'%d',", dev.temperature);
+  }
+
+  mg_printf(conn, "],");
+
+  mg_printf(conn, "}");
+}
+
 static int begin_request_handler(struct mg_connection *conn)
 {
   const struct mg_request_info* req_info = mg_get_request_info(conn);
   int processed = 1;
 
-  if(strcmp(req_info->uri, "/query/asdf") == 0) {
+  if(strcmp(req_info->uri, "/ajax/update") == 0) {
+    ajax_send_update(conn);
+  } else if(strcmp(req_info->uri, "/query/info") == 0) {
     // TODO
   } else {
     processed = 0;
@@ -40,9 +68,15 @@ struct server* server_new(struct monitor* mon)
   return srv;
 }
 
-// Run in a new thread
 void server_start(struct server* srv)
 {
+  if(monitor != NULL) {
+    fprintf(stderr, "This shouldn't have happened");
+    exit(1);
+  }
+
+  monitor = srv->mon;
+
   if((srv->ctx = mg_start(&srv->callbacks, NULL, MG_OPTIONS)) == NULL) {
     fprintf(stderr, "Server start failed, bailing out.");
 
@@ -50,7 +84,6 @@ void server_start(struct server* srv)
 
     return;
   }
-
 }
 
 void server_destroy(struct server* srv)
