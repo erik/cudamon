394653fddde2941776cf27db15cb0fb3aaf68a26
Erik Price <erik.price16@gmail.com>
2013-02-18 23:10:49 -0600

    Allow graphs to be resorted

--- www/cudamon.js
+++ www/cudamon.js
@@ -106,7 +106,8 @@ function addGraph(key, obj) {
 
     if(graphs[key].series.length == 0) {
         console.log('No GPUs support "' + key + '" disabling...');
-        $('#graphs-list').append('<li><del>' + key + '</del></li>');
+        $('#graphs-list').append('<li class="disabled"><del>' + key +
+                                 '</del></li>');
         return;
     }
 
@@ -164,7 +165,8 @@ function addGraph(key, obj) {
 	legend: legend
     });
 
-    $('#graphs-list').append('<li><a href="#' + key +'">' + name + '</a></li>');
+    $('#graphs-list').append('<li id="' + key + '" class="drag"><a href="#' +
+                             key +'">' + name + '</a></li>');
 
     graph.render();
     graphs[key].graph = graph;
@@ -200,5 +202,21 @@ addGraph('clock',
            tickFormat: function(y) { return y + "MHz"; },
            getData: function(dev) { return dev.clock.graphics; }});
 
-
 setInterval(ajaxUpdate, 1000);
+
+// Update our charts to match the newly sorted list
+$('.sortable').sortable({items: '.drag'}).bind('sortupdate', function() {
+    var container = $('#body');
+    var graphs = container.children();
+    container.empty();
+
+    var origPos = {};
+    for(var i in graphs) {
+        origPos[graphs[i].id] = graphs[i];
+    }
+
+    $('#graphs-list .drag').each(function(i, li) {
+        var id = li.getAttribute('id');
+        container.append(origPos[id]);
+    });
+});
